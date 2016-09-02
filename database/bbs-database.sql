20e220d8075bc3fa0248fb9093142ba864d744cb
Fukuoka <fukuoka@Fukuoka-no-MacBook-Pro.local>
2016-09-02 16:56:34 +0900

    bbs-databaseのupdated_atとcreated_atを差し替えました。

--- database/bbs-database.sql
+++ database/bbs-database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:8889
--- Generation Time: 2016 年 9 月 02 日 08:44
+-- Generation Time: 2016 年 9 月 02 日 09:55
 -- サーバのバージョン： 5.5.42
 -- PHP Version: 7.0.8
 
@@ -24,15 +24,15 @@ CREATE TABLE `topics` (
   `id` int(11) NOT NULL,
   `name` varchar(20) NOT NULL,
   `comments` varchar(30) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
 
 --
 -- テーブルのデータのダンプ `topics`
 --
 
-INSERT INTO `topics` (`id`, `name`, `comments`, `created_at`, `updated_at`) VALUES
+INSERT INTO `topics` (`id`, `name`, `comments`, `updated_at`, `created_at`) VALUES
 (1, 'テストの人', 'ああああああ', '2016-09-01 16:35:48', '0000-00-00 00:00:00'),
 (2, '福岡秀明', 'テストです。', '2016-09-01 16:37:02', '0000-00-00 00:00:00'),
 (3, 'フクオカ2', 'もう一回テスト', '2016-09-01 16:37:21', '0000-00-00 00:00:00'),
