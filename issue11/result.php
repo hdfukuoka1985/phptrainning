42d9783ba85958116d1bc75eb370d38839b72ec2
Fukuoka <fukuoka@Fukuoka-no-MacBook-Pro.local>
2016-09-06 02:35:42 +0900

    issue11を更新しました。

--- issue11/result.php
+++ issue11/result.php
@@ -1,32 +1,46 @@
+<?php
+    // データベース接続
+    try {
+    $pdo = new PDO('mysql:host=localhost;dbname=bbs;charset=utf8','fukuoka2','pass888',
+    array(PDO::ATTR_EMULATE_PREPARES => false));
+    } catch (PDOException $e) {
+     exit('データベース接続失敗。'.$e->getMessage());
+    }
+
+
+    // データ引き出し
+    // SELECT文を変数に格納
+    $sql = 'SELECT name, comments FROM topics';
+
+    
+    // SQLステートメントを実行し、結果を変数に格納
+    $stmt = $pdo->query($sql);
+
+
+
+    // foreach文で配列の中身を一行ずつ出力
+    foreach ($stmt as $row) {
+
+    // データベースのフィールド名で出力
+    echo ('投稿者：'.$row['name'].'<br>');
+    echo ('コメント：'.$row['comments'].'<br>');
+
+    }
+?>
+
+
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>BBSの作成</title>
 </head>
 <body>
-<h3>投稿一覧</h3>
-<?php
-try {
-$pdo = new PDO('mysql:host=localhost;dbname=bbs;charset=utf8','fukuoka2','pass888',
-array(PDO::ATTR_EMULATE_PREPARES => false));
-} catch (PDOException $e) {
- exit('データベース接続失敗。'.$e->getMessage());
-}
+    <h3>投稿一覧</h3>
 
-$stmt = $pdo->query("SELECT name, comments FROM topics ORDER BY no ASC");
-while($row = $stmt -> fetch(PDO::FETCH_ASSOC)) {
- $name = $row["name"];
- $comments = $row["comments"];
+        <p>投稿者名：<?php echo $row['name'];?></p>
+        <p>コメント：<?php echo $row['comments'];?></p>
 
-echo<<<EOF
 
-print($name);
-print($comments);
-
-EOF;
-}
-
-?>
-<p><a href="index.html">トップへ戻る</a></p>
+    <p><a href="index.html">トップへ戻る</a></p>
 </body>
 </html>
