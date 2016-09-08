46bc55df0177a11ec725d0513700ed0431882e11
Fukuoka <fukuoka@Fukuoka-no-MacBook-Pro.local>
2016-09-08 17:28:33 +0900

    try~catchの箇所のエラーに関して見て頂きたいです。

--- issue11/regist.php
+++ issue11/regist.php
@@ -2,8 +2,18 @@
     // データベース接続
 
     try {
+    // データベース接続。文字エンコーディング（utf-8）も実施
     $pdo = new PDO('mysql:host=localhost;dbname=bbs;charset=utf8','fukuoka2','pass888',
+
+    // プリペアドステートメントのエミュレーションを無効にして、
+    // MySQL ネイティブの静的プレースホルダを使用する
      array(PDO::ATTR_EMULATE_PREPARES => false));
+
+    // 例外クラスをthrowする
+    throw new Exception('エラーが発生しました');
+
+
+    // throwされた内容をcatchして処理を実行
      } catch (PDOException $e) {
       exit('データベース接続失敗。'.$e->getMessage());
      }
@@ -17,11 +27,18 @@
 
 
     // 変数をデータベースに入れる
-    $sql = 'INSERT INTO topics (name,comment,created_at) VALUES ("'.$name.'","'.$comment.'","'.$timestamp.'")';
-    $stmt =$pdo->prepare($sql);
+    $stmt = $pdo -> prepare("INSERT INTO topics (name, comment,created_at) VALUES (:name, :comment, :timestamp)");
+
+
+    // パラメータに文字列としてバインド（文字列以外入力できなくする）
+    $stmt->bindParam(':name', $name, PDO::PARAM_STR);
+    $stmt->bindParam(':comment', $comment, PDO::PARAM_STR);
+    $stmt->bindParam(':timestamp', $comment, PDO::PARAM_STR);
+
+    // statementを実行
     $stmt -> execute();
 
-
+    // データベースの接続を解除
     $pdo = null;
 
     echo '更新完了しました';
