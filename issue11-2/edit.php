118c90c3a194e7d67bbac0ab761828654b5d5a4a
Fukuoka <fukuoka@Fukuoka-no-MacBook-Pro.local>
2016-09-14 20:03:40 +0900

    BBSの機能が完成しました！

---
+++ issue11-2/edit.php
@@ -0,0 +1,57 @@
+<?php
+    // データベース接続
+    try {
+    $pdo = new PDO('mysql:host=localhost;dbname=bbs;charset=utf8','fukuoka2','pass888',
+     array(PDO::ATTR_EMULATE_PREPARES => false));
+     } catch (PDOException $e) {
+      exit('データベース接続失敗。'.$e->getMessage());
+     }
+
+
+    // データ引き出し
+    // SELECT文を変数に格納
+    $sql = 'SELECT id, name, comment, updated_at FROM topics WHERE 1';
+    $stmt = $pdo->prepare($sql);
+    $stmt ->execute();
+
+
+
+    // データベース接続解除
+    $pdo = null;
+
+?>
+
+
+
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>BBSの作成</title>
+</head>
+<body>
+
+<h3>コメントの編集</h3>
+
+<form action="update-edit.php" method="post">
+
+
+ 
+    <p><?php echo '投稿ID：'.$_GET['id'];?></p>
+    <input type="hidden" name="id" value="<?php echo $_GET['id'];?>" />
+
+    <p><?php echo '変更する名前：'.$_GET['name'];?></p>
+    <input type="hidden" name="name-before" value="<?php echo $_GET['name'];?>" />
+
+    <p><?php echo '変更後の名前：';?></p>
+    <input type="text" name="name-after" size="30"/>
+
+    <input type="submit" value="変更する" />
+
+
+</form>
+
+    <p><a href="index.html">トップへ戻る</a></p>
+    <p><a href="result.php">一覧を見る</a></p>
+
+</body>
+</html>
---
+++ issue11-2/update-delete.php
@@ -0,0 +1,46 @@
+
+<?php
+    // データベース接続
+    try {
+    $pdo = new PDO('mysql:host=localhost;dbname=bbs;charset=utf8','fukuoka2','pass888',
+     array(PDO::ATTR_EMULATE_PREPARES => false));
+     } catch (PDOException $e) {
+      exit('データベース接続失敗。'.$e->getMessage());
+     }
+
+
+    // result.phpから投稿されたデータを変数に入れる
+
+
+    $id = $_POST['id'];
+    $namebefore = $_POST['name-before'];
+    $nameafter = $_POST['name-after'];
+    $namedelete = $_POST['name-delete'];
+
+
+    // DELETE文を実行
+    $sql = "DELETE FROM topics WHERE id='$id'";
+    $stmt = $pdo->prepare($sql);
+    $stmt ->execute();
+
+
+    // データベースから切断
+    $pdo = null;
+
+
+?>
+
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>BBSの作成</title>
+</head>
+<body>
+    <h3>更新しました。</h3>
+
+
+    <p><a href="index.html">トップへ戻る</a></p>
+    <p><a href="result.php">一覧を見る</a></p>
+
+</body>
+</html>
